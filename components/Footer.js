61013ed53f804c0da08bb3759c2c8de3fc15c694
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-01-03 15:52:25 +0100

    Airbnb frontend clone with React

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="grid md:grid-cols-3 lg:grid-cols-4 w-full gap-y-10 px-10 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800 max-w-[200px]">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Combating discrimination</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxury</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Try hosting</p>
+        <p>AirCover:Protection for Hosts</p>
+        <p>How to host responsibly</p>
+        <p>Visit our community forum</p>
+        <p>Help center</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
