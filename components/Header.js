cca4126cee09930459aa726fb35b893177632a98
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-01-09 23:50:39 +0100

    Built functional search page

--- components/Header.js
+++ components/Header.js
@@ -11,12 +11,30 @@ import { useState } from "react";
 import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
+import { Router, useRouter } from "next/router";
 
-function Header() {
+function Header({placeholder}) {
   
   const [searchInput, setSearchInput] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const router = useRouter();
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests
+      },
+    })
+    console.log(router.query.startDate);
+  }
+
+  
 
   const selectionRange = {
     startDate: startDate,
@@ -28,13 +46,17 @@ function Header() {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
   }
+
+  const resetSearch = () => setSearchInput("")
   
   return (
 
     <header className="sticky top-0 z-50 grid grid-cols-3 lg:grid-cols-3 bg-white shadow-md p-5 md: px-10">
       
       {/* Left */}
-      <div className="hidden relative lg:flex items-center h-10 cursor-pointer my-auto ">
+      <div onClick={() => router.push("/") }
+        className="hidden relative lg:flex items-center h-10 cursor-pointer my-auto "
+      >
         <Image 
           src="https://links.papareact.com/qd3" 
           layout="fill"
@@ -50,7 +72,7 @@ function Header() {
           onChange={(e) => setSearchInput(e.target.value)}
           className="pl-2 w-[150px] flex-grow bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your search"
+          placeholder={placeholder || "Start your search"}
         />
 
         <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer lg:inline-flex "/>
@@ -69,6 +91,7 @@ function Header() {
       {searchInput && (
         <div className="flex-col col-span-3 mx-auto mt-10">
           <DateRangePicker
+            dateFormat="MM-DD-YYYY"
             ranges={[selectionRange]}
             onChange={handleSelect}
             minDate={new Date()}
@@ -78,12 +101,12 @@ function Header() {
           <div className="flex items-center border-b mb-4">
             <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
             <UsersIcon className="h-5"/>
-            <input type="number" className="w-12 pl-2 text-lg outline-none text-red-400" min="1"/>
+            <input onChange={(e) => setNoOfGuests(e.target.value)} type="number" value={noOfGuests} className="w-12 pl-2 text-lg outline-none text-red-400" min="1"/>
           </div>
 
           <div className="flex">
-            <button className="grow text-gray-500">Cancel</button>
-            <button className="grow text-red-400">Search</button>
+            <button className="grow text-gray-500" onClick={resetSearch}>Cancel</button>
+            <button onClick={search} className="grow text-red-400">Search</button>
           </div>
         </div>
       )}
